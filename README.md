616c2a7fc0913487b38744171303289a07f3d7f6
pat-jpnk <jpnksoftware@protonmail.com>
2021-10-28 17:51:13 -0400

    first

---
+++ README.md
@@ -0,0 +1,14 @@
+### Finding the maximum element with different classes of algorithmic approaches
+
+The webpage below describes how to solve the problem of finding 
+the maximum value in an array, illustrating different approaches
+
+- brute force
+- divide and conquer 
+- decrease and conquer 
+- transform and conquer
+- use space 
+- dynamic programming 
+- greedy method
+
+source: https://www.cs.yale.edu/homes/aspnes/pinewiki/AlgorithmDesignTechniques.html
---
+++ maximum.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+arr = [2,1,8,6,5,11,3,4,9]
+
+def brute_force(arr):
+  for i in arr:
+    found = True
+    for j in arr:
+      if(i >= j):
+        continue
+      else:
+        found = False
+    if(found):
+      return i
+
+  return None;
+
+
+def divide_and_conquer(arr):
+  length = len(arr)
+  if(length == 1):
+    return arr[0]
+
+  i_1 = length // 2
+  a_1 = arr[0:i_1]
+  m_1 = max(a_1)
+
+  a_2 = arr[i_1:length]
+  m_2 = max(a_2)
+
+  return max(m_1,m_2)
+
+
+
+def dynamic_programming(arr):
+  length = len(arr)
+  b = arr[0::]
+  b[0] = arr[0]
+  
+  for i in range(2,length):
+   if(b[i-1] > arr[i]):
+      b[i] = b[i-1]
+   else:
+      b[i] = arr[i]
+
+  return b[length-1]
+
+
+def greedy_method(arr):
+  maximum = arr[0]
+  for i in range(1,len(arr)):
+    if(arr[i] > maximum):
+      maximum = arr[i]
+  return maximum
+
+
+print(greedy_method(arr));
+
+print(dynamic_programming(arr));
+
+print(divide_and_conquer(arr));
+
+print(brute_force(arr));
